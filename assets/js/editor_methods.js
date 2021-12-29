c46de2a8a06e74166362de2ede8a8140cc429bec
SmokedSalmon <jerrysu.jie@gmail.com>
2021-12-30 02:05:20 +0800

    FontAwesome 5 + code split + 2 new some feature minor fixes

---
+++ assets/js/editor_methods.js
@@ -0,0 +1,164 @@
+ // Post setup actions
+ function hideAllPopups () {
+  document.querySelectorAll('.popup').forEach(el => { el.blur(); el.style.visibility = 'hidden'; })
+}
+
+/* Draw table drop down */
+
+
+function drawTable (editor, row, column, options = {}) {
+  const { hasHeaderRow } = options;
+  let tableStr = '\n\n';
+  tableStr = tableStr.concat(new Array(column)
+      .fill()
+      .reduce((pre, value, index) => `${pre} ${hasHeaderRow ? 'HEADER': 'Column'} ${index + 1} |`, '|'));
+  tableStr = tableStr.concat('\n');
+  tableStr = tableStr.concat(new Array(column).fill().reduce((pre, value, index) => `${pre} -------- |`, '|'));
+  tableStr = tableStr.concat('\n');
+  for (let i = 0; i < row; i++) {
+      tableStr = tableStr.concat(new Array(column).fill().reduce((pre, value, index) => `${pre}          |`, '|'));
+      tableStr = tableStr.concat('\n');
+  }
+  tableStr = tableStr.concat('\n');
+  editor.codemirror.doc.replaceRange(tableStr, editor.codemirror.getCursor('to'));
+  editor.codemirror.focus();
+}
+
+function prepareDrawTableDropdown(editor) {
+  // Customized draw table action
+  const drawTableMenuButton = document.querySelector('button.table[title="Table"]');
+  const drawTableMenu = document.querySelector('#draw-table-popup');
+  const drawTableMocked = document.querySelector('#draw-table-popup table.draw-table-mockup');
+  const drawTableToggleHeaderSwitcher = drawTableMenu.querySelector('#draw-table-toggle-header');
+  const drawTableFirstRow = drawTableMenu.querySelector('table.draw-table-mockup tr:first-child');
+  const drawTableApplyButton = drawTableMenu.querySelector('button.apply');
+  Popper.createPopper(drawTableMenuButton, drawTableMenu, {
+      placement: 'bottom-start',
+  }); 
+  
+  drawTableMenuButton.addEventListener('click', (e) => {
+      const visibilityToChange = ['hidden', ''].includes(drawTableMenu.style.visibility) ? 'visible' : 'hidden';
+      hideAllPopups();
+      e.stopPropagation()
+      drawTableMenu.style.visibility = visibilityToChange;
+      drawTableMenu.addEventListener('click', (e) => e.stopPropagation());
+  })
+  drawTableApplyButton.addEventListener('click', () => { editor.codemirror.focus(); drawTableMenu.style.visibility = 'hidden'});
+  drawTableToggleHeaderSwitcher.addEventListener('change', (e) => {
+      const headers = new Array(...drawTableFirstRow.querySelectorAll('th'));
+      const nonHeaders = new Array(...drawTableFirstRow.querySelectorAll('td'));
+      headers.forEach(el => el.style.display = drawTableToggleHeaderSwitcher.checked ? 'table-cell' : 'none');
+      nonHeaders.forEach(el => el.style.display = drawTableToggleHeaderSwitcher.checked ? 'none' : 'table-cell');
+  });
+
+  // table cell click
+  const mockedUpTableBody = drawTableMocked.children[0];
+  const maxRowCount = mockedUpTableBody.childElementCount;
+  const maxColumnCount = mockedUpTableBody.children[1].childElementCount;
+  function cellInteraction (e) {
+    const cell = e.target || e.srcElement, row = cell.parentElement;
+    const rowCount = new Array(...mockedUpTableBody.children).indexOf(row) + 1;
+    const columnCount = new Array(...row.children).filter(el => el.style.display !=='none').indexOf(cell) + 1;
+    switch (e.type) {
+        case 'mouseover':
+            for (let x = 0; x < maxRowCount; x++) {
+                for (let y = 0; y < maxColumnCount; y++) {
+                    const loopingRow = mockedUpTableBody.children[x];
+                    const element = loopingRow.children[y];
+                    if (x >= rowCount)  element.classList.remove('included');
+                    else if (y >= columnCount) element.classList.remove('included');
+                    else element.classList.add('included');
+                    // for 1st row
+                    if (x === 0) {
+                        if (y >= columnCount) loopingRow.children[y + maxColumnCount].classList.remove('included');
+                        else loopingRow.children[y + maxColumnCount].classList.add('included');
+                    }
+                }
+            }
+            break;
+        case 'click':
+            console.log(`${rowCount} - ${columnCount}`);
+            drawTable(editor, rowCount, columnCount, { hasHeaderRow: drawTableToggleHeaderSwitcher.checked });
+            break;
+    }
+  };
+
+  new Array(...mockedUpTableBody.children).forEach(row => {
+      new Array(...row.children).forEach(cell => {
+          cell.addEventListener('mouseover', cellInteraction)
+          cell.addEventListener('click', cellInteraction)
+      })
+  })
+}
+
+/* Customized guide/help menu */
+function prepareGuideMenu() {
+  const guideMenuButton = document.querySelector('button.help[title="Help"]');
+  const guideMenu = document.querySelector('#guide-popup');
+  Popper.createPopper(guideMenuButton, guideMenu, {
+      placement: 'bottom-start',
+  }); 
+  guideMenuButton.addEventListener('click', (e) => {
+      const visibilityToChange = ['hidden', ''].includes(guideMenu.style.visibility) ? 'visible' : 'hidden';
+      hideAllPopups();
+      e.stopPropagation()
+      guideMenu.style.visibility = visibilityToChange;
+      guideMenu.addEventListener('click', (e) => e.stopPropagation());
+  })
+}
+
+
+
+/* Other customized actions */
+function newParagraph(editor) {
+  editor.codemirror.doc.replaceRange('\n\n', editor.codemirror.getCursor('to'));
+  editor.codemirror.focus();
+}
+
+function newLine(editor) {
+    editor.codemirror.doc.replaceRange('  \n', editor.codemirror.getCursor('to'));
+    editor.codemirror.focus();
+  }
+
+function addTaskListExample(editor) {
+  const content = '\n\n- [ ] task to finish\n- [x] finished task\n\n';
+  editor.codemirror.doc.replaceRange(content, editor.codemirror.getCursor('to'));
+  editor.codemirror.focus();
+}
+
+function addMultilineCodeBlock(editor) {
+  const content = '\n\n\`\`\`\nAdd your codes here\n\`\`\`\n\n';
+  editor.codemirror.doc.replaceRange(content, editor.codemirror.getCursor('to'));
+  editor.codemirror.focus();
+}
+
+/* Control panel */
+function prepareControlPanel() {
+  const session_prefix = btoa('stitch_doc');
+  sessionStorage[`${session_prefix}_github-domain`] = 'https://github.com';
+  sessionStorage[`${session_prefix}_repository-path`] = 'SmokedSalmon/markdown_editor';
+  sessionStorage[`${session_prefix}_branch`] = 'master';
+  sessionStorage[`${session_prefix}_file-path`] = 'index.html';
+  
+  const doneButton = document.getElementById('copy-and-go');
+  doneButton.addEventListener('click', () => {
+      const githubDomain = sessionStorage[`${session_prefix}_github-domain`];
+      const repositoryPath = sessionStorage[`${session_prefix}_repository-path`];
+      const branch = sessionStorage[`${session_prefix}_branch`];
+      const filePath = sessionStorage[`${session_prefix}_file-path`];
+      const newWindow = window.open(`${githubDomain}/${repositoryPath}/edit/${branch}/${filePath}`);
+      newWindow.onload = () => {
+          newWindow.document.querySelector('input#commit-summary-input').value = 'Update newly edit content';
+          const range = newWindow.document.createRange();
+          const intervalId = setInterval(() => {
+              const codeEditor = newWindow.document.getElementById('code-editor');
+              if (!codeEditor) return;
+              range.selectNodeContents(codeEditor);
+              const sel = newWindow.getSelection();
+              sel.removeAllRanges();
+              sel.addRange(range);
+              clearInterval(intervalId);
+          }, 100)
+      }
+  });
+}
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,26 @@
+function loadScript (url) {
+    const tag = document.createElement('script');
+    tag.setAttribute('src', url);
+    document.head.appendChild(tag);
+}
+
+function loadStyle (url) {
+    const tag = document.createElement('link');
+    tag.setAttribute('rel', 'stylesheet');
+    tag.setAttribute('href', url);
+    document.head.appendChild(tag);
+}
+loadScript('./assets/js/easymde.min.js');
+loadScript('./assets/js/popper.min.js');
+loadScript('./assets/js/editor_methods.js');
+loadStyle('./assets/style/easymde.min.css');
+loadStyle('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/css/all.min.css');
+
 window.onload = function () {
     let editor;
     // setting up the editor
     editor = new EasyMDE({
-        // autoDownloadFontAwesome: false,
+        autoDownloadFontAwesome: false,
         element: document.getElementById('editing-area'),
         autofocus: true,
         initialValue: '# Your title\nWrite some content here',
@@ -10,28 +28,40 @@ window.onload = function () {
 
         // toolbar
         toolbar: [
+            {
+                name: "new-line",
+                action: newLine,
+                className: "fa new-line",
+                title: "New line",
+            },
+            {
+                name: "new-paragraph",
+                action: newParagraph,
+                className: "fa new-paragraph",
+                title: "New paragraph",
+            }, '|',
             'bold', 'italic', 'strikethrough', 'heading', 'heading-bigger', 'heading-smaller',
             {
                 name: "heading set",
-                className: "fa fa-header",
+                className: "fa fa-header fa-heading",
                 title: "Heading set",
                 children: [
                     {
                         name: "heading-1",
                         action: EasyMDE.toggleHeading1,
-                        className: "fa fa-header fa-header-x fa-header-1",
+                        className: "fa fa-header fa-heading",
                         title: "Heading 1",
                     },
                     {
                         name: "heading-2",
                         action: EasyMDE.toggleHeading2,
-                        className: "fa fa-header fa-header-x fa-header-2",
+                        className: "fa fa-header fa-heading",
                         title: "Heading 2",
                     },
                     {
                         name: "heading-3",
                         action: EasyMDE.toggleHeading3,
-                        className: "fa fa-header fa-header-x fa-header-3",
+                        className: "fa fa-header fa-heading",
                         title: "Heading 3",
                     }
                 ]
@@ -72,151 +102,15 @@ window.onload = function () {
         // promptURLs: true
     });
 
-    // Post setup actions
-    const hideAllPopups = () => {
-        document.querySelectorAll('.popup').forEach(el => { el.blur(); el.style.visibility = 'hidden'; })
-    }
+    prepareDrawTableDropdown(editor);
 
-    // Customized draw table action
-    const drawTableMenuButton = document.querySelector('button.table[title="Table"]');
-    const drawTableMenu = document.querySelector('#draw-table-popup');
-    const drawTableMocked = document.querySelector('#draw-table-popup table.draw-table-mockup');
-    const drawTableToggleHeaderSwitcher = drawTableMenu.querySelector('#draw-table-toggle-header');
-    const drawTableFirstRow = drawTableMenu.querySelector('table.draw-table-mockup tr:first-child');
-    const drawTableApplyButton = drawTableMenu.querySelector('button.apply');
-    Popper.createPopper(drawTableMenuButton, drawTableMenu, {
-        placement: 'bottom-start',
-    }); 
-    function drawTable (row, column, options = {}) {
-        const { hasHeaderRow } = options;
-        let tableStr = '\n\n';
-        tableStr = tableStr.concat(new Array(column)
-            .fill()
-            .reduce((pre, value, index) => `${pre} ${hasHeaderRow ? 'HEADER': 'Column'} ${index + 1} |`, '|'));
-        tableStr = tableStr.concat('\n');
-        tableStr = tableStr.concat(new Array(column).fill().reduce((pre, value, index) => `${pre} -------- |`, '|'));
-        tableStr = tableStr.concat('\n');
-        for (let i = 0; i < row; i++) {
-            tableStr = tableStr.concat(new Array(column).fill().reduce((pre, value, index) => `${pre}          |`, '|'));
-            tableStr = tableStr.concat('\n');
-        }
-        tableStr = tableStr.concat('\n');
-        editor.codemirror.doc.replaceRange(tableStr, editor.codemirror.getCursor());
-        editor.codemirror.focus();
-    }
-    drawTableMenuButton.addEventListener('click', (e) => {
-        const visibilityToChange = ['hidden', ''].includes(drawTableMenu.style.visibility) ? 'visible' : 'hidden';
-        hideAllPopups();
-        e.stopPropagation()
-        drawTableMenu.style.visibility = visibilityToChange;
-        drawTableMenu.addEventListener('click', (e) => e.stopPropagation());
-    })
-    drawTableApplyButton.addEventListener('click', () => { editor.codemirror.focus(); drawTableMenu.style.visibility = 'hidden'});
-    drawTableToggleHeaderSwitcher.addEventListener('change', (e) => {
-        const headers = new Array(...drawTableFirstRow.querySelectorAll('th'));
-        const nonHeaders = new Array(...drawTableFirstRow.querySelectorAll('td'));
-        headers.forEach(el => el.style.display = drawTableToggleHeaderSwitcher.checked ? 'table-cell' : 'none');
-        nonHeaders.forEach(el => el.style.display = drawTableToggleHeaderSwitcher.checked ? 'none' : 'table-cell');
-    });
+    prepareGuideMenu();
 
-    // table cell click
-    const mockedUpTableBody = drawTableMocked.children[0];
-    const maxRowCount = mockedUpTableBody.childElementCount;
-    const maxColumnCount = mockedUpTableBody.children[1].childElementCount;
-    const cellInteraction = (e) => {
-        const cell = e.target || e.srcElement, row = cell.parentElement;
-        const rowCount = new Array(...mockedUpTableBody.children).indexOf(row) + 1;
-        const columnCount = new Array(...row.children).filter(el => el.style.display !=='none').indexOf(cell) + 1;
-        switch (e.type) {
-            case 'mouseover':
-                for (let x = 0; x < maxRowCount; x++) {
-                    for (let y = 0; y < maxColumnCount; y++) {
-                        const loopingRow = mockedUpTableBody.children[x];
-                        const element = loopingRow.children[y];
-                        if (x >= rowCount)  element.classList.remove('included');
-                        else if (y >= columnCount) element.classList.remove('included');
-                        else element.classList.add('included');
-                        // for 1st row
-                        if (x === 0) {
-                            if (y >= columnCount) loopingRow.children[y + maxColumnCount].classList.remove('included');
-                            else loopingRow.children[y + maxColumnCount].classList.add('included');
-                        }
-                    }
-                }
-                break;
-            case 'click':
-                console.log(`${rowCount} - ${columnCount}`);
-                drawTable(rowCount, columnCount, { hasHeaderRow: drawTableToggleHeaderSwitcher.checked });
-                break;
-        }
-    };
-    new Array(...mockedUpTableBody.children).forEach(row => {
-        new Array(...row.children).forEach(cell => {
-            cell.addEventListener('mouseover', cellInteraction)
-            cell.addEventListener('click', cellInteraction)
-        })
-    })
+    prepareControlPanel()
 
-    /* Other customized actions */
-    function addTaskListExample(editor) {
-        const content = '\n\n- [ ] task to finish\n- [x] finished task\n\n';
-        editor.codemirror.doc.replaceRange(content, editor.codemirror.getCursor());
-        editor.codemirror.focus();
-    }
-    
-    function addMultilineCodeBlock(editor) {
-        const content = '\n\n\`\`\`\nAdd your codes here\n\`\`\`\n\n';
-        editor.codemirror.doc.replaceRange(content, editor.codemirror.getCursor());
-        editor.codemirror.focus();
-    }
-
-    // Customized guide/help menu
-    const guideMenuButton = document.querySelector('button.help[title="Help"]');
-    const guideMenu = document.querySelector('#guide-popup');
-    Popper.createPopper(guideMenuButton, guideMenu, {
-        placement: 'bottom-start',
-    }); 
-    guideMenuButton.addEventListener('click', (e) => {
-        const visibilityToChange = ['hidden', ''].includes(guideMenu.style.visibility) ? 'visible' : 'hidden';
-        hideAllPopups();
-        e.stopPropagation()
-        guideMenu.style.visibility = visibilityToChange;
-        guideMenu.addEventListener('click', (e) => e.stopPropagation());
-    })
-    
     document.addEventListener('click', hideAllPopups);
-
     // show side-by-side
     editor.toggleSideBySide();
-
-    /* Control panel */
-    const session_prefix = btoa('stitch_doc');
-    sessionStorage[`${session_prefix}_github-domain`] = 'https://github.com';
-    sessionStorage[`${session_prefix}_repository-path`] = 'SmokedSalmon/markdown_editor';
-    sessionStorage[`${session_prefix}_branch`] = 'master';
-    sessionStorage[`${session_prefix}_file-path`] = 'index.html';
-    
-    const doneButton = document.getElementById('copy-and-go');
-    doneButton.addEventListener('click', () => {
-        const githubDomain = sessionStorage[`${session_prefix}_github-domain`];
-        const repositoryPath = sessionStorage[`${session_prefix}_repository-path`];
-        const branch = sessionStorage[`${session_prefix}_branch`];
-        const filePath = sessionStorage[`${session_prefix}_file-path`];
-        const newWindow = window.open(`${githubDomain}/${repositoryPath}/edit/${branch}/${filePath}`);
-        newWindow.onload = () => {
-            newWindow.document.querySelector('input#commit-summary-input').value = 'Update newly edit content';
-            const range = newWindow.document.createRange();
-            const intervalId = setInterval(() => {
-                const codeEditor = newWindow.document.getElementById('code-editor');
-                if (!codeEditor) return;
-                range.selectNodeContents(codeEditor);
-                const sel = newWindow.getSelection();
-                sel.removeAllRanges();
-                sel.addRange(range);
-                clearInterval(intervalId);
-            }, 100)
-        }
-    });
 
     // debug only
     window.editor = editor;
